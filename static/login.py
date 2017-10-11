f12c85bb0fab140dbb59f70bcdb68de91289fb5f
Trupti Jangam <trupti.jangam3@gmail.com>
2017-10-11 16:48:40 +0530

    Delete login.py

--- static/login.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('User.db')
-print "Opened database successfully";
-
-conn.execute('CREATE TABLE Users(USERNAME TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL)')
-print "Table created successfully";
-conn.close()
